6df318af603327bb498c3731c108bbf1fa171016
Fabio Eberlin <fabioeberlin@gmail.com>
2023-02-14 09:26:13 -0300

    Obtendo um usuário por id

--- src/main/java/com/fabioeberlin/workshopmongo/services/UserService.java
+++ src/main/java/com/fabioeberlin/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.fabioeberlin.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fabioeberlin.workshopmongo.domain.User;
 import com.fabioeberlin.workshopmongo.repository.UserRepository;
+import com.fabioeberlin.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
